42b7e6e99b705bdb826dd5d976a5735cf7b85fb9
hjvdz <hossvadi@gmail.com>
2022-11-04 10:40:37 +0100

    Delete data modifications

---
+++ src/JDBCDemo/deleteData.java
@@ -0,0 +1,38 @@
+package JDBCDemo;
+
+import java.sql.*;
+
+public class deleteData {
+    public static void main(String[] args) {
+    	String url = "jdbc:mysql://localhost:3306/demo";
+    	String user = "hjvdz";
+        String pass = "Hj1374@@";
+
+        try {
+            // 1. get the connection
+            Connection myConn = DriverManager.getConnection(url, user, pass);
+
+            // 2. create the statement
+            Statement myStmt = myConn.createStatement();
+            
+            // 3. Execute SQL query
+            String sql = "delete from employees where lastname='John'";
+
+            int rowAffected = myStmt.executeUpdate(sql);
+
+            System.out.println("Rows Affected: " + rowAffected);
+            System.out.println("Delete complete.\n");
+            
+            ResultSet myRs = myStmt.executeQuery("select * from employees");
+            
+            // 4. Proccess the result set
+            while (myRs.next()) {
+            	System.out.println(myRs.getString("lastname") + ", " + myRs.getString("firstname"));
+            }
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
+  
